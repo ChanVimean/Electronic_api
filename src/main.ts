0f8511b9033733bf06955bd9edb7669c499fa9e4
ChanVimean <mrrmeanvlogger@gmail.com>
2025-05-16 21:29:43 +0700

    Adding .editorconfig and enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+// ! Catch Erros
+bootstrap().catch((err) => {
+  console.error('Error starting the application: ', err);
+  process.exit(1);
+});
